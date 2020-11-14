378639dd096be8b716a6c06773906f13b0dee1d2
Rachel Mattson <rachelmattson1008@gmail.com>
2020-11-13 20:48:48 -0500

    create files

---
+++ program/main.cpp
@@ -0,0 +1,52 @@
+#include "ItemType.h"
+#include "BinaryTree.h"
+#include <exception>
+#include <cstddef>
+#include <iostream>
+#include <ostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+
+
+using namespace std;
+
+int main(int argc, char **argv)
+
+// {
+//     cout << "Hello, World" << endl;
+
+//     if (argc <= 1)
+//     {
+//         cout << "Usage: ./main [list_file] " << endl;
+//         return 1;
+//     }
+
+//     BinaryTree * bossTree = new BinaryTree();
+
+//     // Read in item input
+//     ItemType item;
+//     int input;
+//     std::fstream file;
+//     file.open(argv[1], fstream::in);
+
+//     if (file.is_open())
+//     {
+//         //    cout << "opened file" << endl;
+//         while (!file.eof())
+//         {
+//             file >> input;
+
+//             item.initialize(input);
+//             bossTree->insert(item);
+//             //cin.get(); // error handling
+//             // file >> input;
+//         }
+//     }
+//     else
+//     {
+//         cout << "Failed to open the input file" << endl;
+//         return 1;
+//     }
+
+}
---
+++ program/sorting.h
@@ -0,0 +1,11 @@
+#include "ItemType.h"
+
+
+class Sorting {
+int SelectionSort(ItemType[] unsorted);
+int MergeSort(ItemType[] unsorted);
+int QuickSort(ItemType[] unsorted);
+int HeapSort(ItemType[] unsorted);
+}
+
+// Sorting::SelectionSort(raw data)
