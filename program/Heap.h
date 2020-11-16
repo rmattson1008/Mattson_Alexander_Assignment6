086ba527c42b2d1343f76c6d8da1a5f892d6e914
sarahalexander99 <57675110+sarahalexander99@users.noreply.github.com>
2020-11-15 19:07:30 -0500

    Update Heap.h

--- program/Heap.h
+++ program/Heap.h
@@ -5,14 +5,22 @@
 
 class Heap
 {
+  struct HeapType {
+    void ReheapDown(int root, int bottom);
+    void ReheapUp(int root, int bottom);
+    ItemType * elements;
+    int numElements;
+  };
 public:
-// incorrect headers
-    void ReheapUp();
-    void ReHeapDown();
+  // incorrect headers
+  void ReheapUp(int root, int bottom);
+  void ReheapDown(int root, int bottom);
+
 
 private:
-    ItemType * elements;
-    int length;
+  ItemType * elements;
+  int length;
+  int hCount = 0;
 };
 
 #endif
